88be947543dcc7041b078323a71c777499dc1995
Nagma <99428751+NagmaKhattar@users.noreply.github.com>
2024-05-17 10:16:08 -0400

    showing fetch

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -13,6 +13,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * push operation
  * showing you the change on NetBeans and pushing to Github
  changing the file on remote 
+ showing fetch and merge
  * @author dancye May 7, 2018
  */
 public class Student 
