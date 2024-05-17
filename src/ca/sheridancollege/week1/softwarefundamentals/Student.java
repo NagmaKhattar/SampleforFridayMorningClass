87947ca57e8e198672c80e9f3acbfbf74f903143
NagmaKhattar <nagmakhattar@gmail.com>
2024-05-17 09:22:52 -0400

    third commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -10,6 +10,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * each student has: a name (first an last name)
  * each student can: say their name (getName)
  * @modifiedby Nagma
+ * just to see change
  * 
  * @author dancye May 7, 2018
  */
